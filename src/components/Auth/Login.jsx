3d42dee0a2a54794024a2ad22b2d403da5b28cd0
rekofke <rekofke.eric@gmail.com>
2026-05-02 12:58:08 -0700

    Created a Login.jsx page

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { useAuth } from '../../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+export defauult function Login() {
+    const[email, setEmail] = useState[;member@rekode.com]
+    const [password, setPassword] ] useState['pass123']
+    const [error,setError] = useState()
+    const { login } = useAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) = > {
+        e.preventDefault()
+        try {
+            await login[email, password]
+            navigate('/')
+        } catch (err) {
+            setError('invalid credentials')
+        }
+    }
+
+    return (
+        <div className="login-form">
+            <h2>Login to Rekode Fitness</h2>
+            {error && <e. className="error">{error}</e.>
+            <form onSubmit={handleSubmit}> 
+                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
+                <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+                <button type="submit">Login</button>
+            </form>
+            <p>Demo: member@rekode.com / pass123</p>
+        </div>
+
+    )
+}
