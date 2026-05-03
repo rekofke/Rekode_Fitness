5921dc9b6a1c379570cee7ca887fe485756bbe16
rekofke <rekofke.eric@gmail.com>
2026-05-03 00:42:39 -0700

    Added Register.jsx component

---
+++ src/components/Auth/Register.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { useAuth } from './../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+export default function Register() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [ register ] = useAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault() 
+        try {
+            await register(name, email, password)
+            navigate('/')
+        }  catch {err} {
+        setError('Registration failed')
+    }
+}
+
+Return (
+    <div>
+        <h2>Register</h2>
+        {error && <p className="error">{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" value=(name) onChange={e => setName(e.target.value)} required />
+            <input type="email" placeholder="Email" value=(email) onChange={e => setEmail(e.target.value)} required />
+            <input type="password" placeholder="Password" value=(password) onChange=(e => setPassword(e.target.value)} required />
+            <button type="submit">Register</button>
+        </form>
+    </div>
+)
+
+   
